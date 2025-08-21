74b4c1d5736817183c8bfb436dfce0b37a47cc8d
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2025-08-21 01:58:37 -0300

    ini_proj
    Iniciado o projeto 8sem

---
+++ trab8sem_flutter_web/lib/painel/painel.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class Painel extends StatelessWidget {
+  const Painel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(title: Text("Trabalhando aqui"), ),
+
+
+    );
+  }
+}
